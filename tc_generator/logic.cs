7dc5a97cda834abed324109447c16ab87a88b074
prithaupadhyay <prithaupadhyay@gmail.com>
2018-12-24 14:14:36 +0530

    Changes As Suggested

--- tc_generator/logic.cs
+++ tc_generator/logic.cs
@@ -1,15 +1,8 @@
 using System;
-
-namespace HelloWorldApplication {
-   
-   class HelloWorld {
-      
+namespace LogicApplication {
+   class Logic {
       static void Main(string[] args) {
-         Console.WriteLine("Hello World");
-         Console.ReadKey();
-         
          int t = Convert.ToInt32(Console.ReadLine());
-         
          while(t>0)
          {
              long n,x,res;
