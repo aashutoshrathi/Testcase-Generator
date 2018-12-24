baf19c93af76eac5ad955722af96da3a87c3de5b
prithaupadhyay <prithaupadhyay@gmail.com>
2018-12-24 19:25:13 +0530

    lang_compiler.py created

--- tc_generator/tc_gen.py
+++ tc_generator/tc_gen.py
@@ -15,6 +15,8 @@ import zipfile
 
 import math
 
+from lang_compiler import LANGS
+
 if sys.version[0] == '3':
     INPUT = input
     XRange = range
@@ -27,33 +29,6 @@ except OSError:
 
 POWER = math.pow
 RINT = random.randint
-
-LANGS = [{'req': 'gcc',
-          'command': './logic',
-          'link': 'https://gcc.gnu.org/install/',
-          'compile': 'gcc -o logic logic.c'},
-         {'req': 'g++',
-          'command': './logic',
-          'link': 'https://www.cs.odu.edu/~zeil/cs250PreTest/latest/Public/installingACompiler/',
-          'compile': 'g++ -o logic logic.cpp'},
-         {'req': 'Java',
-          'command': 'java logic',
-          'link': 'https://introcs.cs.princeton.edu/java/15inout/windows-cmd.html',
-          'compile': 'javac logic.java'},
-         {'req': 'Python',
-          'command': 'python logic.py',
-          'link': 'https://www.python.org/downloads/',
-          'compile': ''},
-         {'req': 'C#',
-          'command': 'mono logic',
-          'link': 'https://www.mono-project.com/docs/about-mono/languages/csharp/',
-          'compile': 'msc logic.cs'},
-         {'req': 'Golang',
-          'command': './logic',
-          'link': 'https://golang.org/doc/install',
-          'compile': 'go build logic.go'},
-         ]
-
 
 def generate(choice, i):
     try:
@@ -86,8 +61,8 @@ def zip_them(test_files, choice):
 
 def main():
     choice = int(INPUT(
-        "Enter your choice of language\n1 for C\n2 for C++\n3 for Java\n4 for Python\n5 for C#\n6 for Golang"))
-    if choice not in range(1, 6):
+        "Enter your choice of language\n1 for C\n2 for C++\n3 for Java\n4 for Python\n5 for C#\n6 for Golang\n"))
+    if choice not in range(1, 7):
         print("Wrong choice entered!")
         exit()
 
