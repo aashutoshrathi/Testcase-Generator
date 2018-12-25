a7d3a23625c11eebb6617602c4a01bc4897079a8
prithaupadhyay <prithaupadhyay@gmail.com>
2018-12-25 12:42:38 +0530

    autopep8 done

--- tc_generator/test_generate.py
+++ tc_generator/test_generate.py
@@ -1,8 +1,8 @@
 import os
 from tc_gen import generate
 
+
 def check_zip():
-    generate(4,1)           #test choice==4
+    generate(4, 1)  # test choice==4
     if os.system('rm -r test-cases.zip') == 0:
         assert(True)
-        
