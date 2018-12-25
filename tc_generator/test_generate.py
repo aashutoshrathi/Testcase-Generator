26e108fda840158e43a85a7ad15a8fada0d0e83d
prithaupadhyay <prithaupadhyay@gmail.com>
2018-12-25 12:34:29 +0530

    New line added and extra file deleted

--- tc_generator/test_generate.py
+++ tc_generator/test_generate.py
@@ -5,3 +5,4 @@ def check_zip():
     generate(4,1)           #test choice==4
     if os.system('rm -r test-cases.zip') == 0:
         assert(True)
+        
--- tests/test_generate.py
+++
@@ -1,7 +0,0 @@
-import os
-from tc_gen import generate
-
-def check_zip():
-    generate(4,1)           #test choice==4
-    if os.system('rm -r testcases.zip') == 0:
-        assert(True)
