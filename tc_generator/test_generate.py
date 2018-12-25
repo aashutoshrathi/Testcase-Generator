cfd5c95df32284b4a8887c3804893f0f64d1fe64
prithaupadhyay <prithaupadhyay@gmail.com>
2018-12-25 12:31:34 +0530

    test-cases.zip name corrected

--- tc_generator/test_generate.py
+++ tc_generator/test_generate.py
@@ -3,5 +3,5 @@ from tc_gen import generate
 
 def check_zip():
     generate(4,1)           #test choice==4
-    if os.system('rm -r testcases.zip') == 0:
+    if os.system('rm -r test-cases.zip') == 0:
         assert(True)
