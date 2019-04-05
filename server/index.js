667e44b24bbffd80934ce01806a9837389edd874
harish chennupati <y17cs027@rvrjc.ac.in>
2019-04-05 15:36:53 +0530

    added a route for file upload

--- server/index.js
+++ server/index.js
@@ -1,24 +1,47 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 
-const compile = require("./routes/api/compile");
-const zip = require("./routes/api/zip");
-const generate = require("./routes/api/generate");
-
+const compile = require('./routes/api/compile');
+const zip = require('./routes/api/zip');
+const generate = require('./routes/api/generate');
+const upload = require('express-fileupload');
 const app = express();
 
 //Database config goes here
 
 //Database connection goes here
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.use(upload());
+app.get('/', (req, res) => {
+	res.send('Hello World');
+});
+app.get('/fileupload', function(req, res) {
+	console.log(__dirname);
+	res.sendFile(__dirname + '/index.html');
+});
+app.post('/upload', function(req, res) {
+	console.log(req.files);
+	var file = req.files.upfile;
+	if (file) {
+		var filename = file.name;
+		var uploadpath = __dirname + '/uploads/' + filename;
+		file.mv(uploadpath, function(err) {
+			if (err) {
+				console.log('file upload Failed', filename, err);
+				res.send('Error Occured!');
+			} else {
+				console.log('File Uploaded', filename);
+				return res.redirect('/api/compile');
+			}
+		});
+	} else {
+		res.send('Sorry please select the file to upload');
+	}
 });
 
 //Compile, generate and zip routes
-app.use("/api/compile", compile);
-app.use("/api/zip", zip);
-app.use("/api/generate", generate);
+app.use('/api/compile', compile);
+app.use('/api/zip', zip);
+app.use('/api/generate', generate);
 
 const port = process.env.PORT || 5000;
 
--- server/routes/api/compile.js
+++ server/routes/api/compile.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
 // @route api/compile
 // @desc For now test route
 // @access Public
-router.get("/", (req, res) => {
-  res.send("Compile Works");
+router.get('/', (req, res) => {
+	res.send('Compiling ...');
 });
 
 module.exports = router;
