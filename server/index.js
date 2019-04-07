40efd03a7852f85842937bff0f2f8d5780a014ba
harish chennupati <y17cs027@rvrjc.ac.in>
2019-04-07 13:57:40 +0530

    updated server/index.js

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.post('/upload', function(req, res) {
 	if (file) {
 		var filename = file.name;
 		var timestamp = new Date().getTime();
-		var uploadpath = __dirname + '/uploads/' + filename + timestamp;
+		var uploadpath = __dirname + '/uploads/' + timestamp + '-' + filename;
 		var dir = __dirname + '/uploads'; //added a check wheteher the directory exists or not
 		if (!fs.existsSync(dir)) {
 			fs.mkdirSync(dir);
