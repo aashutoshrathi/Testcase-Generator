f8933e3ba34384a01e717123a18c6f08ded98aa2
harish chennupati <y17cs027@rvrjc.ac.in>
2019-05-12 13:19:14 +0530

    executing scripts as soon as user uploaded

--- server/routes/api/compile.js
+++ server/routes/api/compile.js
@@ -6,6 +6,13 @@ const router = express.Router();
 // @desc For now test route
 // @access Public
 router.get('/', (req, res) => {
+	const { spawnSync } = require('child_process');
+	const pythonScript = spawnSync('python3', ['tc_generator/tc_gen.py', '4']);
+	console.log(pythonScript.stderr.toString('utf-8'));
+	if (0 == pythonScript.status) {
+		// no error, so exit code is 0
+		const pythonOutput = pythonScript.stdout.toString('utf8'); // .stdout is generally  a binary buffer
+	}
 	res.send('Compiling ...');
 });
 
