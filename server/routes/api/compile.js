ba213a9eeeb07dde1ed6f4b225db8cbd3e118bd9
harish chennupati <y17cs027@rvrjc.ac.in>
2019-05-13 12:34:53 +0530

    fixed issue

--- server/routes/api/compile.js
+++ server/routes/api/compile.js
@@ -8,12 +8,14 @@ const router = express.Router();
 router.get('/', (req, res) => {
 	const { spawnSync } = require('child_process');
 	const pythonScript = spawnSync('python3', ['tc_generator/tc_gen.py', '4']);
-	console.log(pythonScript.stderr.toString('utf-8'));
+
 	if (0 == pythonScript.status) {
 		// no error, so exit code is 0
 		const pythonOutput = pythonScript.stdout.toString('utf8'); // .stdout is generally  a binary buffer
+	} else {
+		console.log(pythonScript.stderr.toString('utf-8'));
 	}
-	res.send('Compiling ...');
+	res.redirect('/api/zip');
 });
 
 module.exports = router;
