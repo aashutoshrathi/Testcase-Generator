3e7b2b24c7d2b44fc6d861e8c1f4519d3f66d89a
harish chennupati <y17cs027@rvrjc.ac.in>
2019-05-14 19:56:52 +0530

    squashed commits

--- server/routes/api/compile.js
+++ server/routes/api/compile.js
@@ -6,7 +6,16 @@ const router = express.Router();
 // @desc For now test route
 // @access Public
 router.get('/', (req, res) => {
-	res.send('Compiling ...');
+	const { spawnSync } = require('child_process');
+	const pythonScript = spawnSync('python3', ['tc_generator/tc_gen.py', '4']);
+
+	if (0 == pythonScript.status) {
+		// no error, so exit code is 0
+		const pythonOutput = pythonScript.stdout.toString('utf8'); // .stdout is generally  a binary buffer
+	} else {
+		console.log(pythonScript.stderr.toString('utf-8'));
+	}
+	res.redirect('/api/zip');
 });
 
 module.exports = router;
