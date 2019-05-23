dbc45cbfd80984a8f4ccc0abd6fd816ded625c8e
harish chennupati <42904380+mark-nakachon@users.noreply.github.com>
2019-05-23 23:33:34 +0530

    Add Husky and Travis (#105)
    * set automatic-code-fixing using eslint prettier husky lint-staged editorconfig
    
    * Added new line to .editorconfig
    
    * Added new line t
    
    * Done with this
    
    * Added new line
    
    * Added new line
    
    * added travis
    
    * fixed with eslint
    
    * fixed error in travis
    
    * fixed errors using eslint

--- server/routes/api/compile.js
+++ server/routes/api/compile.js
@@ -1,24 +1,22 @@
-const express = require('express');
+const express = require('express')
 
-const router = express.Router();
+const router = express.Router()
 
-const { authenticated } = require('../../helpers/authentication');
+const { authenticated } = require('../../helpers/authentication')
 
 // @route api/compile
 // @desc For now test route
 // @access Public
-
 router.get('/', authenticated, (req, res) => {
-	const { spawnSync } = require('child_process');
-	const pythonScript = spawnSync('python3', ['tc_generator/tc_gen.py', '4']);
+  const { spawnSync } = require('child_process')
+  const pythonScript = spawnSync('python3', ['tc_generator/tc_gen.py', '4'])
 
-	if (0 == pythonScript.status) {
-		// no error, so exit code is 0
-		const pythonOutput = pythonScript.stdout.toString('utf8'); // .stdout is generally  a binary buffer
-	} else {
-		console.log(pythonScript.stderr.toString('utf-8'));
-	}
-	res.redirect('/api/zip');
-});
-
-module.exports = router;
+  if (pythonScript.status === 0) {
+    // no error, so exit code is 0
+    pythonScript.stdout.toString('utf8') // .stdout is generally  a binary buffer
+  } else {
+    console.log(pythonScript.stderr.toString('utf-8'))
+  }
+  res.redirect('/api/zip')
+})
+module.exports = router
--- server/routes/api/generate.js
+++ server/routes/api/generate.js
@@ -1,14 +1,12 @@
-const express = require('express');
-
-const router = express.Router();
-
-const { authenticated } = require('../../helpers/authentication');
+const express = require('express')
+const router = express.Router()
+const { authenticated } = require('../../helpers/authentication')
 
 // @route api/generate
 // @desc For now test route
 // @access Public
 router.get('/', authenticated, (req, res) => {
-	res.send('Generate Works');
-});
+  res.send('Generate Works')
+})
 
-module.exports = router;
+module.exports = router
